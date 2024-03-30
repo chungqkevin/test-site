7386d77ddaefee01b7826d0bf250f03297261997
Kevin Chung <chung.q.kevin@gmail.com>
2024-03-29 17:52:26 -0700

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-white py-2 font-display font-medium tracking-wide leading-relaxed mb-2 flex flex-col lg:flex-row justify-between items-center">
       <span className="mb-2 lg:mb-0">© Papillon</span>
-      <a href="#" className="text-black hover:opacity-50 lg:mr-4">Join the maillist</a>
+      <button className="text-black hover:opacity-50 lg:mr-4">Join the maillist</button>
     </footer>
   );
 }
